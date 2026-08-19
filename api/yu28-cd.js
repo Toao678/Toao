486758a30f8b0209da7b957326c990e37558429a
Toao678 <166302617@qq.com>
2026-08-20 01:17:14 +0800

    更新 yu28-cd.js

--- api/yu28-cd.js
+++ api/yu28-cd.js
@@ -1,16 +1,9 @@
-const fetch = require('node-fetch');
+if (!response.ok) {
+    return res.status(response.status).json({ error: `Upstream API error: ${response.status}` });
+}
 
-const API_KEY = "yu28_dd02fb963ac7590c";
-
-module.exports = async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  try {
-    const response = await fetch("https://yu28.top/api/kj?nbr=1", {
-      headers: { "X-Api-Key": API_KEY }
-    });
-    const data = await response.json();
-    res.json(data);
-  } catch(e) {
-    res.status(500).json({ error: e.message });
-  }
-};
+const data = await response.json();
+res.json(data);
+} catch (e) {
+res.status(500).json({ error: e.message });
+}
